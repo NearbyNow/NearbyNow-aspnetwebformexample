f15e6357a3ece7d289f10f3a1019a344e3f33d0a
Billy Coover <billycoover@gmail.com>
2020-07-27 13:54:38 -0700

    Misc changes and FAQ

--- NearbyNowWebFormsExample/BasePage.cs
+++ NearbyNowWebFormsExample/BasePage.cs
@@ -8,6 +8,7 @@ namespace NearbyNowWebFormsExample
     public class BasePage : System.Web.UI.Page
     {
         protected const string api = "http://localhost:50899/plugin/nationwideserviceareareviewcombo?storefronttoken={0}&state={1}&city={2}&zoomlevel={3}&showmap={4}&techemail={5}&mapscrollwheel={6}&fblike={7}&fbcomment={8}&agent={9}&count={10}&lat={11}&long={12}&mapsize={13}&cluster={14}&ShowMiniMap={15}&ReviewPinMax={16}";
+
         protected const string StorefrontToken = "b345df54-41ae-4e27-a65f-59eaca9a7445";
     }
 }
---
+++ NearbyNowWebFormsExample/Faq.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NearbyNowWebFormsExample
+{
+    public partial class Faq : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var wc = new WebClient();
+
+            var html = wc.DownloadString(
+                String.Format("http://localhost:50899/plugin/faq?storefronttoken={0}&topic={1}&count={2}",
+                HttpUtility.UrlEncode(StorefrontToken),
+                HttpUtility.UrlEncode("mobile-apps"),// topic
+                3 //count
+                )
+            );
+
+            NearbyNowLiteral.Text = html;
+        }
+    }
+}
